715382a0d3cc0706b43a8e8adf2076f86f78c093
JustMe <justme1236499@gmail.com>
2026-06-27 23:42:38 +0530

    replace hard coded values with variables

--- src/main/java/raytracer/Main.java
+++ src/main/java/raytracer/Main.java
@@ -3,17 +3,26 @@ import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main (String[] args) throws FileNotFoundException {
-        PrintWriter write = new PrintWriter("test.ppm");
-        write.println("P3 400 300 255");
-        for(int i=0; i<300; i++) {
-            for(int j=0; j<400; j++) {
-                int r = (int)((j/399.0) * 255);
-                int g = (int)((i/299.0) *255);
-                int b = 0;
-                write.println(r + " " + g + " " + b);
+    public static void main (String[] args){
+
+        final int IMG_WIDTH = 400;
+        final int IMG_HEIGHT = 300;
+        final int MAX_VAL = 255;
+        try (PrintWriter write = new PrintWriter("test.ppm")) {
+            write.println("P3");
+            write.println(IMG_WIDTH);
+            write.println(IMG_HEIGHT);
+            write.println(MAX_VAL);
+            for (int i = 0; i < IMG_HEIGHT; i++) {
+                for (int j = 0; j < IMG_WIDTH; j++) {
+                    int r = (int) ((j / (IMG_WIDTH-1.0)) * MAX_VAL);
+                    int g = (int) ((i / (IMG_HEIGHT-1.0)) * MAX_VAL);
+                    int b = 0;
+                    write.println(r + " " + g + " " + b);
+                }
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open file: " + e.getMessage());
         }
-        write.close();
     }
 }
